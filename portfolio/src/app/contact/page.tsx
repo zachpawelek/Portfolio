e59f3f34e4f1a798ed9416c4e5fc7cfdafb9b3f8
zachpawelek <zachpawelek@proton.me>
2025-12-13 02:47:22 -0600

    About, Contact, Projects, Placeholder pages

--- portfolio/src/app/contact/page.tsx
+++ portfolio/src/app/contact/page.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+
 export default function ContactPage() {
-    return <main className="p-6">Contact</main>
+  return (
+    <main className="relative min-h-screen isolate overflow-hidden">
+        <div className="absolute inset-0 z-0">
+        <Image
+          src="/images/birds.jpg" 
+          alt="Bird background"
+          fill
+          priority
+          quality={90}
+          sizes="100vw"
+          className="object-cover"
+        />
+        </div>
+
+        {/* Optional: content on top of the background */}
+        <div className="relative z-10 p-6 text-white">
+          Contact
+        </div>
+    </main>
+  );
 }
